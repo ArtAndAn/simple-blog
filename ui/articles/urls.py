44a444321dc603e2d580b4a09948b8c25fe70226
Andriy Artamonov <andreiartamonovaa@gmail.com>
2021-11-03 11:26:55 +0200

    Code refactoring All_articles view was separated to 3 different views (all_articles, topic_articles, author_articles) All_articles template was separated to 3 different templates (all_articles, category_articles, author_articles) Articles_base template was added for all articles templates except check.html Create_cards.js was deleted

--- ui/articles/urls.py
+++ ui/articles/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.all_articles, name='all_articles'),
     path('check/', views.check, name='data_check'),
-    path('user/<str:author>', views.all_articles, name='user_articles'),
-    path('topic/<str:topic>', views.all_articles, name='topic_articles'),
+    path('user/<str:author>', views.author_articles, name='user_articles'),
+    path('topic/<str:topic>', views.topic_articles, name='topic_articles'),
     path('article/<str:slug>', views.single_article, name='single_article')
 ]
--- ui/articles/views.py
+++ ui/articles/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 
 
-def all_articles(request, topic=None, author=None):
-    if topic:
-        context = {'api_link': f'/api/category/{topic}', 'content': topic}
-    elif author:
-        context = {'api_link': f'/api/user/{author}', 'content': author}
-    else:
-        context = {'api_link': '/api/', 'content': 'All'}
+def all_articles(request):
+    context = {'api_link': '/api/', 'content': 'All'}
     return render(request, 'articles/all_articles.html', context=context)
 
 
+def topic_articles(request, topic):
+    context = {'api_link': f'/api/category/{topic}', 'content': topic}
+    return render(request, 'articles/category_articles.html', context=context)
+
+
+def author_articles(request, author):
+    context = {'api_link': f'/api/user/{author}', 'content': author}
+    return render(request, 'articles/author_articles.html', context=context)
+
+
 def single_article(request, slug):
-    return render(request, 'articles/single_article.html', context={'url': f'article/{slug}'})
+    return render(request, 'articles/single_article.html', context={'api_link': f'/api/article/{slug}'})
 
 
 def check(request):
     return render(request, 'articles/check.html', context={'url': '/api/check'})
-
